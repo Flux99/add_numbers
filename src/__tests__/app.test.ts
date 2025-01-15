7d587e23f885fd291ccf2afaa7884139a8edca0b
Flux99 <9901anonymous@gmail.com>
2025-01-16 01:24:28 +0530

    now add func is adding two or multiple numbers and written passing test cases

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -4,7 +4,12 @@ describe("String Calculator", () => {
   it("should return 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
-  
+  it("should return the sum of two numbers", () => {
+    expect(add("1,5")).toBe(6);
+  });
+  it("should return the sum of multiple numbers", () => {
+    expect(add("1,2,3,4,5")).toBe(15);
+  });
     
 });
 
